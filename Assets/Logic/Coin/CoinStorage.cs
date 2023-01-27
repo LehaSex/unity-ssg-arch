edd2f857f1d68c1742d383269a8e8b936042a6d7
LehaSex <94061794+LehaSex@users.noreply.github.com>
2023-01-28 01:22:04 +0300

    Vector2 and Vector3 save support

--- Assets/Logic/Coin/CoinStorage.cs
+++ Assets/Logic/Coin/CoinStorage.cs
@@ -1,19 +1,17 @@
 public class CoinStorage : Storage
 {
-    FileBasedPrefsPC fileBasedPrefsPC = new FileBasedPrefsPC();
     public int coins;
 
     private const string KEY = "coins";
 
     public override void Init()
     {
-        this.coins = fileBasedPrefsPC.GetInt(KEY, 0);
+        this.coins = JSONSave.GetInt(KEY, 0);
     }
 
     public override void Save()
     {
-        fileBasedPrefsPC.SetInt(KEY, this.coins);
-        fileBasedPrefsPC.Save();
+        JSONSave.SetInt(KEY, this.coins);
     }
 
 }
--- Assets/Logic/Tester.cs
+++ Assets/Logic/Tester.cs
@@ -9,6 +9,13 @@ public class Tester : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        JSONSave.Start(new JSONSaveConfig
+        {
+            SaveFileName = "data.json",
+            AutoSaveData = true,
+            ScrambleSaveData = false,
+            EncryptionSecret = "123",
+        });
         this.coinStorage = new CoinStorage();
         this.coinStorage.Init();
         this.coinService = new CoinService(this.coinStorage);
@@ -16,6 +23,13 @@ public class Tester : MonoBehaviour
 
         Debug.Log("Coin Service Initialized. Coins: " + this.coinService.coins);
         Debug.Log("Data Path: " + Application.persistentDataPath + "/data.json");
+        Vector3 v2 = new Vector3(7, 7, 7);
+        v2 = JSONSave.GetVector3("test2", v2);
+        Debug.Log("Vector3: " + v2.x + ", " + v2.y + ", " + v2.z);
+        JSONSave.SetVector2("test2", new Vector2(6, 5));
+        Debug.Log("Vector2: " + JSONSave.GetVector2("test2", new Vector2(1, 1)).x + ", " + JSONSave.GetVector2("test2", new Vector2(1, 1)).y);
+
+
     }
 
     // Update is called once per frame
